e1e740a05fa193819501a8120ba8228816f128d2
Filip <fpetrovic92@gmail.com>
2018-09-20 19:38:54 +0200

    Java main method

--- src/main/java/org/cancergenomicscloud/EntryPoint.java
+++ src/main/java/org/cancergenomicscloud/EntryPoint.java
@@ -1,12 +1,40 @@
 package org.cancergenomicscloud;
 
+import org.cancergenomicscloud.adapter.http.UnirestHttpClient;
+import org.cancergenomicscloud.adapter.output.SystemOutStringOutput;
+import org.cancergenomicscloud.cli.CgcCli;
+import org.cancergenomicscloud.cli.CgcCliBuilder;
+import org.cancergenomicscloud.cli.formatter.JsonResponseFormatter;
+import org.cancergenomicscloud.cli.formatter.ResponseFormatter;
+import org.cancergenomicscloud.cli.handler.ListProjectsCommand;
+import org.cancergenomicscloud.cli.http.HttpClient;
+import org.cancergenomicscloud.cli.output.StringOutput;
+import org.cancergenomicscloud.cli.parser.CliArgumentsParser;
+
 /**
  * Created by Filip.
  */
 
 public class EntryPoint {
 
-	public static void main(String [] args) {
+	public static void main(String [] args) throws Exception {
+		final HttpClient httpClient = new UnirestHttpClient();
+		final ResponseFormatter formatter = new JsonResponseFormatter();
+		final StringOutput output = new SystemOutStringOutput();
+
+		try {
+
+			final CgcCli cgcCli = new CgcCliBuilder(new CliArgumentsParser())
+					.withHandler("projects list", new ListProjectsCommand(httpClient, formatter, output))
+					.get();
+
+			cgcCli.execute(args);
+
+		} catch (Exception e) {
+			output.print(e.getMessage());
+		} finally {
+			httpClient.shutdown();
+		}
 
 	}
 
